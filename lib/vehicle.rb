3b67a9e80af6fbe1b49fad4a6f764088c8ad8f00
stephen <lonelytreesl@yahoo.com>
2015-05-06 19:27:58 -0700

    add #worth_buying() to vehicle.rb and spec, all specs passing.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,10 +1,16 @@
 class Vehicle
   define_method(:initialize) do |make, model, year|
     @year = year
+    @make = make
   end
 
   define_method(:age) do
     current_year = Time.new().year()
     current_year.-(@year)
   end
+  define_method(:worth_buying) do
+    false
+    #american_made = ["Ford", "Chevy",]
+  end
+
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -8,4 +8,10 @@ describe(Vehicle) do
       expect(test_vehicle.age()).to(eq(15))
     end
   end
+  describe('#worth_buying') do
+    it('returns false when the vehicle is not american made and younger than 16 years') do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.worth_buying()).to(eq(false))
+      end
+    end
 end
